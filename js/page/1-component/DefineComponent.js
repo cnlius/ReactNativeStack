cb3fb36b20fe9c093a84129d675b263f7cd265d7
Jason <cnmrliu@163.com>
2017-10-18 23:04:17 +0800

    定义组件

---
+++ js/page/1-component/DefineComponent.js
@@ -0,0 +1,17 @@
+/**
+ * @author liusong
+ * @date 2017/10/18
+ */
+
+import React,{Component} from 'react'
+import {
+    View,
+    Text
+}from 'react-native'
+
+class DefineComponent extends Component{
+
+    render(){
+        return <View></View>
+    }
+}
