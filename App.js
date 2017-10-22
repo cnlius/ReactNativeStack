42977d30b0c126aa9a0b28ff5666415bcde07695
Jason <cnmrliu@163.com>
2017-10-22 22:27:06 +0800

    5.helloworld

--- App.js
+++ App.js
@@ -4,54 +4,35 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1, //项目等分剩余空间
+        justifyContent: 'center', //flex子项在主轴（横轴）方向上的对齐方式：位于容器中心；
+        alignItems: 'center',  //flex子项在flex容器的当前行的侧轴（纵轴）方向上的对齐方式
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
 });
--- js/page/0-helloworld/HelloWorld.js
+++ js/page/0-helloworld/HelloWorld.js
@@ -2,3 +2,42 @@
  * @author liusong
  * @date 2017/10/18
  */
+
+//组件导入区：导入开发需要的组件
+
+//导入React，和React中的Component组件
+import React,{Component} from 'react'
+import {
+    StyleSheet, //样式表
+    Text,
+    View
+} from 'react-native'
+
+export default class HelloWorld extends Component<{}>{
+    // Component<{}>: 0.49版本更改？？？
+
+    //render方法是必须的方法，返回最终渲染出来界面的JSX语句
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>
+                    Hello World
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, //项目等分剩余空间
+        justifyContent: 'center', //flex子项在主轴（横轴）方向上的对齐方式：位于容器中心；
+        alignItems: 'center',  //flex子项在flex容器的当前行的侧轴（纵轴）方向上的对齐方式
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
